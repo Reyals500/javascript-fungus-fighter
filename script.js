f83cf8101c1927887d70d25682bb576fe9ee3d28
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2023-12-05 10:39:15 -0600

    Got the object set up now moving on to the function

--- script.js
+++ script.js
@@ -3,6 +3,30 @@
 // Example:
 // let fungusHP = 100;
 
+//creating an object array with all the attack names 
+let attack = [
+{
+    name: 'Arcane Septar',
+    apCost: 12,
+    hpDamage: 14
+},
+{
+    name: 'Entangle',
+    apCost: 23,
+    hpDamage: 9
+},
+{
+    name: 'Dragon Blade',
+    apCost: 38,
+    hpDamage: 47
+},
+{
+    name: 'Star Fire',
+    apCost: 33,
+    hpDamage: 25
+}
+]
+
 function onReady() {
     console.log("Ready to go!")
     
@@ -18,3 +42,10 @@ function onReady() {
 
 
 onReady()
+
+//start by making a function
+function onAttackCommand(event){
+    event.preventDefault()
+    console.log('Inside the attack function');
+
+}
